fb0c9ec8f5300ff0fb9a06ace6ee0b81e12285e6
Saw85san <120392660+Saw85san@users.noreply.github.com>
2022-12-13 06:57:45 +0300

    Add files via upload
    Homework 1 Part 2 Template

---
+++ Home work2.py
@@ -0,0 +1,39 @@
+
+###Home work2####
+number=[ ]
+a=int(input("enter the nuber 1"))
+b=int(input("enter the numer 2"))
+sum=(a+b)
+print(sum)
+##############
+
+rows =4
+def add(n1: int, n2: int,n3:int,n4:int)
+print(add)
+i = 0
+tru=True
+fal=False
+while(i <= rows):
+    j = 1
+    if n4 <n1,n3<n1,n2<n1:
+           j = n4 + 1
+           j = n3+ 1
+           j = n2+ 1
+           j = n1+ 1
+    i = i + 1
+    print(tru)
+    else:
+       print(fal)
+############
+
+b=int(input("Enter the base"))
+h=int(input("enter the height"))
+a=0.5*b*h
+print(a)
+#############
+
+p=int(input("Enter the num1"))
+q=int(input("enter the num2"))
+a=(p%q)
+print("The rimainder is :",a)
+
